83f5e07e50b5adcf89382a629905be90274037a0
Aaryan <72549723+aaryan-clis@users.noreply.github.com>
2021-12-28 10:10:23 +0530

    Initial

---
+++ Greater number.C
@@ -0,0 +1,15 @@
+#include <cstdio>
+int main ()
+ 
+{
+    int a , b ; // declearing valriables 
+    printf("Enter A value : "); // printing for "Enter A value "
+    scanf("%d",&a); // storing of A value 
+    printf("Enter B value :"); //printing for "Enter B value"
+    scanf("%d",&b); // storaing value of B value
+    if (a>b)   // used if else statement 
+    printf("The vaue of  A is bigger than B");
+    else 
+    printf("The value of B is bigger then A  ");
+    
+}
